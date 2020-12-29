2d0cea22216b7ff4cb085fc01ca95bba45e02042
Jesse Miner <jesse@givegab.com>
2020-12-29 17:45:49 -0500

    Name image files with movie ids

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,5 @@
 class Movie < ActiveRecord::Base
   def image
-    if title == 'The Black Panther'
-      'black_panther.jpg'
-    elsif title == 'Kindergarten Teacher'
-      'The_Kindergarten_Teacher.jpg'
-    end
+    "images/movie#{id}.jpg"
   end
 end
